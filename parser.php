48ed06b5ca0d689de193f61cbaf6173e390f87c1
Petr <fizzik@ya.ru>
2021-12-26 11:10:39 +0300

    Create parser.php

---
+++ parser.php
@@ -0,0 +1,10 @@
+<?
+function Parse($p1, $p2, $p3) {
+    $num1 = strpos($p1, $p2);
+    if ($num1 === false) return 0;
+    $num2 = substr($p1, $num1);
+    return strip_tags(substr($num2, 0, strpos($num2, $p3)));
+}
+ 
+$String = file_get_contents('http://site.ru');
+echo Parse($String, '<div class="unique item">', '</div>');
